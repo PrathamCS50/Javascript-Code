98c2c7552c28aaf59dba68d6526293e30ae19f79
PrathamCS50 <prathamgoel2002@gmail.com>
2024-07-25 18:55:09 +0530

    DataTypes

---
+++ 02DataTypes/test.js
@@ -0,0 +1,18 @@
+"use Strict"; // Use all the js code according to the new standards
+
+// alert("Hello Boii"); // We use NodeJS and not browser
+// High Code Readebility
+
+let name = "Pratham";
+let age = 22;
+let loggedIn = true;
+let state = null;
+let babe;
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof loggedIn); // boolean
+console.log(typeof state); // object (typeof null returns "object" due to a historical bug in JavaScript)
+console.log(typeof babe); // undefined
+console.log(typeof null); // OBJECT
+console.log(typeof undefined); // undefined

e2c28e7b375a53ff4987f821cfd6dea32ca1bee4
PrathamCS50 <prathamgoel2002@gmail.com>
2024-07-26 08:55:49 +0530

    Strings Concepts

---
+++ 01basics/05Striings.js
@@ -0,0 +1,20 @@
+// const name = prompt("Enter Your Name ");
+// const age = prompt("Enter Your Age ");
+
+// console.log(`Your name is ${name} and You are ${age} years old`);
+
+// const newName = new String("Pratham");
+// console.log(newName);
+
+const gamename = new String("PrathamYT");
+console.log(gamename[0]);
+console.log(gamename.__proto__); //Object
+
+const newstr = gamename.substring(2, 5);
+console.log(newstr);
+console.log(gamename.length);
+console.log(gamename.slice(-8, 9));
+
+const nam = "Pratham Goel";
+console.log(nam);
+console.log(nam.trim());
